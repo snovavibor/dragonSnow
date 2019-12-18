43c07c3280721062f89c71c9a3cca069b4d80a93
snovavibor <bzos@protonmail.com>
2019-12-19 01:53:00 +0200

    remodification code

--- js/dragon-snow.js
+++ js/dragon-snow.js
@@ -1,116 +1,96 @@
 (function($){
-    $(function(){
+  $(function(){
 
-/**
- * фабрика запускает снежинки каждые 400 мс
- * name & ind используется для определения каждой снежинки
- */
+
 class Dragon_Snow {
-    constructor(){
-        this.name = 'snow';
-        this.ind = 1;
-        let timer = setInterval(()=>{
-            this.ind++;
-            this.render_snow();
-        },400)
-    }
+
+  constructor(container,size_snow){
+      this.container = container;
+      this.size_snow = size_snow;
+      this.name_id = 'chesnokit_snow';
+      this.ind = 1;
+      this.timer = '';
+      
+  }
 
 
-/**
- * функция определяет координату по оси х появления снежинки
- * за счет рандомного значения взятого из длины блока (в данном случае wrap)
- */
-    rnd_snow(){
-        let w_snow = $('.dragon_snow').width()-50;
-       let x1 = this.getRandomInt(w_snow); 
-        return x1;
-      }
-    
-    getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-      }
+  start_make_snow(){
+      this.timer = setInterval(()=>{
+          this.ind++;
+          this.render_snow();
+      },400)
+  }
 
-/**
- * добавляет элемент снежинка в блок wrap и задает свойства этому элементу
- */
-    render_snow(){
-          
-        $('.dragon_snow').append('<span class="rr" id="'+this.name+this.ind+'">&#10052;</span>');
-        $('#'+this.name+this.ind).css({
-            'color':'white',
-            'font-size':'30px',
-            'position':'absolute',
-            'top': '-30px',
-            'left':this.rnd_snow() +'px'
-        })
-        /**
-         * после прорисовки вызывается функция движения элемента
-         */
-        this.fly_snow(this.name+this.ind);
+  stop_make_snow(){
+      clearInterval(this.timer);
+      
   }
 
   /**
-   * функция полета(движения) снежинки
-   * @param {string} name 
-   */
-  fly_snow(name){
+* функция определяет координату по оси х появления снежинки
+* за счет рандомного значения взятого из длины блока (в данном случае wrap)
+*/
+rnd_snow(){
 
-    let y = $('.dragon_snow').height();
-    /**
-     * step_move шаг движения элемента вниз
-     */
-    let step_move = 5;
+  let width_container = $(this.container).width()-50;
+ let x_position = this.getRandomInt(width_container); 
+  return x_position;
+}
 
-    /**
-     * step_visible шаг уменьшения видимости элемента (эффект таяния -исчезновение)
-     * длина пути делится на шаг полета,100/ на это значение = количество отрезков
-     * и далее вычисляется шаг понижения видимости 
-     */
-    let step_visible = (1/100)*(100/(y/step_move));
+getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-    /**
-     * flag_move флаг для опрделения пути движения, чтобы остановить элемент и удалить его
-     */
-      let flag_move = 0;
+/**
+* добавляет элемент снежинка в блок wrap и задает свойства этому элементу
+*/
+render_snow(){
+
+  let snow_element =$("<span>",{
+      "class":"chesnokit_snow",
+      "id":this.name_id+this.ind,
+      html:"\&#10052;"
+  }).css({
+      'color':'white',
+      'font-size':this.size_snow+'px',
+      'position':'absolute',
+      'z-index':1000,
+      'top': -this.size_snow+'px',
+      'left':this.rnd_snow() +'px'
+  }).appendTo(this.container);
         
-      /**
-       * пока элемент не долетел до низа окна браузера минус 50px, он двигается с шагом step_move , после прохождения
-       * этой точки он исчезает и удаяется
-       */
-       if(flag_move < y-50){ 
+  
+  /**
+   * после прорисовки вызывается функция движения элемента
+   */
+  this.fly_snow($(snow_element).attr('id'));
+}
 
-          flag_move+=step_move;
-     
-          /**
-           * движение за счет функции анимации: задается конечная точка y-40px(40px от конечной точки вверх чтобы не
-           * увеличивать окно браузера когда элемент дойдет вниз, у него же есть и свой размер)
-           * прозрачность можно задать ранее вычисленным шагом step_visible, или же просто указать конечную прозрачность
-           * разные эффекты: таким образом элемент исчезает в пути, в другом случае долетает до низа и там исчезает
-           */
-          $('#'+name).animate({
-            'top':y-40,
-            
-            'opacity':step_visible
-            },30000, function(){
-              $('#'+name).fadeOut(300).remove();
-             })
+/**
+ * функция полета(движения) снежинки
+ * @param {string} name 
+ */
+fly_snow(name){
 
-        }else{
+  let y = $(this.container).height();
+  
+        $('#'+name).animate({
+          'top':y-(this.size_snow+10),
+          
+          'opacity':0.05
+          },30000, function(){
+            $('#'+name).fadeOut(300).remove();
+           })
 
-          $('#'+name).fadeOut().remove();
-         
-        }    
-
-  }     
+}     
 
 
 }
 
-   new Dragon_Snow();
+
+let dragon_snow = new Dragon_Snow($('.dragon_snow'),30);
+dragon_snow.start_make_snow();
 
 
-
-
-
-    });
+  });
 })(jQuery);
